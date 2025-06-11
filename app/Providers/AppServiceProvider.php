2538c9cb59097e15f2a82a719f06dbce5f44a197
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2025-06-11 21:19:25 +0300

    Change Edit pages allover the project to open as slideOver instead of model once i hash it in its resource getPages method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Tables\Actions\EditAction;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        EditAction::configureUsing(function ($action) {
+            return $action->slideOver()/*->form(fn (Model $record) => $record::getFormSchema())
+                ->mutateFormDataUsing(fn (array $data, Model $record) => $record->fill($data))
+                ->successNotificationTitle('Record updated successfully')*/ ;
+        });
     }
 }
