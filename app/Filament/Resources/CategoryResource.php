f2c3b28ebf5c5139f0573a2b2be78797813708f0
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2025-06-18 17:18:49 +0300

    Finish Category form and table with its filters

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -17,30 +17,47 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema(Category::getFormSchema());
-    }
+    protected static ?string $navigationGroup = 'Shop';
+
+    protected static ?int $navigationSort = 4;
 
     public static function table(Table $table): Table
     {
         return $table
+            //->query(fn(Builder $query) => $query->with('parent'))
             ->columns([
                 Tables\Columns\TextColumn::make('name')
+                    ->sortable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('slug')
-                    ->searchable(),
-                Tables\Columns\ImageColumn::make('image'),
+
                 Tables\Columns\TextColumn::make('parent.name')
-                    ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+                Tables\Columns\ImageColumn::make('image'),
+
+                Tables\Columns\TextColumn::make('description')
+                    ->html()
+                    ->limit(35)
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+
                 Tables\Columns\IconColumn::make('is_active')
-                    ->boolean(),
+                    ->label('Active')
+                    ->boolean()
+                    ->sortable(),
+
                 Tables\Columns\IconColumn::make('is_visible')
-                    ->boolean(),
+                    ->label('Visibility')
+                    ->boolean()
+                    ->sortable(),
+
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -50,18 +67,48 @@ class CategoryResource extends Resource
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
+            ->defaultSort('updated_at', 'desc')
             ->filters([
-                //
+                Tables\Filters\TernaryFilter::make('is_active')
+                    ->label('Active')
+                    ->placeholder('All')
+                    ->native(false)
+                    ->trueLabel('Yes')
+                    ->falseLabel('No'),
+
+                Tables\Filters\TernaryFilter::make('is_visible')
+                    ->label('Visible')
+                    ->placeholder('All')
+                    ->native(false)
+                    ->trueLabel('Yes')
+                    ->falseLabel('No'),
+
+                Tables\Filters\SelectFilter::make('parent_id')
+                    ->relationship('parent', 'name')
+                    ->label('Parent Category')
+                    ->searchable()
+                    ->preload()
+                    ->placeholder('All')
+                    ->native(false),
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema(Category::getFormSchema());
     }
 
     public static function getRelations(): array
